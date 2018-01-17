a94a6743849f674a7bc316250bca16ac613fd227
Geurt Lagemaat <geurtlagemaat@gmail.com>
2018-01-17 20:16:14 +0100

    req. moved to sep. file

--- setup.py
+++ setup.py
@@ -1,14 +1,5 @@
 from distutils.core import setup
 
-
-install_requires = [
-    'twisted==16.4.1',
-    'circus==0.14.0'
-    'concurrentloghandler>=0.9.1'
-    'paho-mqtt==1.3.1'
-	'astral'
-]
-
 setup(name='bliknetlib',
       version='1.0.5',
       description='Python IoT integration utilities',
@@ -18,5 +9,4 @@ setup(name='bliknetlib',
       packages=['bliknetlib'],
       package_dir={'bliknetlib': 'src/bliknetlib'},
       package_data={'bliknetlib': ['data/*.dat']}
-	  # py_modules=['nodeControl', 'CircusNotifier', 'serialMsg', 'serialMsgQueues', 'serialNodesProtocol', 'astro_functions'],
       )
